bf037dda8d8972000e032da8fe8dada057b274e3
Ramesh  Muthuraj <ramesh_muthuraj@trimble.com>
2019-01-02 14:38:31 +0530

    wss agent config added

---
+++ wss_agent.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+
+curl -LJO https://github.com/rameshtrimble/whitesource/blob/master/whitesource-fs-agent-18.11.2.1.jar
+
+curl -LJO https://github.com/rameshtrimble/whitesource/blob/master/whitesource-fs-agent.config
+
+java -jar whitesource-fs-agent.jar "$@"
